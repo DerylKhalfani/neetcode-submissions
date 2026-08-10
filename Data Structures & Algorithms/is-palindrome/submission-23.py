80201e23b10a952d6a1d518852542d264f8f9da6
Muhammad Derylian Khalfani <119742306+DerylKhalfani@users.noreply.github.com>
2026-08-10 22:54:04 +0200

    Add: is-palindrome - submission-23

---
+++ Data Structures & Algorithms/is-palindrome/submission-23.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        s = "".join(s.split())
+        l,r = 0, len(s) - 1
+        
+        while l < r:
+
+            while not s[r].isalpha() and not s[r].isdigit():
+                r -= 1
+                if r < l:
+                    return True
+
+            while not s[l].isalpha() and not s[l].isdigit():
+                l += 1
+                if l > r:
+                    return True
+
+            if s[l].lower() == s[r].lower():
+                l += 1
+                r -= 1
+                continue
+            
+            if l > r:
+                return True
+            else:
+                return False
+        
+        return True
